fb2328c6b813e1e395008cac2d3653b4f5ca7ea4
Daekyung Kim <timkim0713@gmail.com>
2021-05-22 15:42:59 +0900

    google maps added in call

--- application/BottomTabScreens/Call/CallScreen.js
+++ application/BottomTabScreens/Call/CallScreen.js
@@ -7,15 +7,39 @@ import {
   Text,
   View,
 } from 'react-native';
+
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+
 import FA5Icon from 'react-native-vector-icons/FontAwesome5';
 import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import FAIcon from 'react-native-vector-icons/FontAwesome';
 import AntIcon from 'react-native-vector-icons/AntDesign';
 
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
+
 export default function CallScreen() {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>CallScreen!</Text>
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        style={styles.map}
+        region={{
+          latitude: 37.5665,
+          longitude: 126.978,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}></MapView>
     </View>
   );
 }
