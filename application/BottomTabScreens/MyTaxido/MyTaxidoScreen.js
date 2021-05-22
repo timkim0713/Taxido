e07c7f1258cac4c11ab7ccc564f6094dcee87516
Youngho Kim <44015667+rxYoungho@users.noreply.github.com>
2021-05-22 15:44:48 +0900

    myTaxido

--- application/BottomTabScreens/MyTaxido/MyTaxidoScreen.js
+++ application/BottomTabScreens/MyTaxido/MyTaxidoScreen.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+
+import { StyleSheet, ScrollView, Alert, RefreshControl, ActivityIndicator, Image } from 'react-native';
+
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
+  Button,
   View,
-} from 'react-native';
+  Container,
+  Text,
+  Item,
+  Header,
+  Title,
+  Content,
+  Input,
+  Left,
+  Right,
+  Body,
+  Icon,
+  ListItem,
+  Thumbnail,
+} from 'native-base';
+
+
 import FA5Icon from 'react-native-vector-icons/FontAwesome5';
 import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import FAIcon from 'react-native-vector-icons/FontAwesome';
@@ -17,8 +32,48 @@ const THEME_COLOR = '#242424';
 
 export default function MyTaxidoScreen() {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>MyTaxidoScreen!</Text>
-    </View>
+    <Container>
+      <Header searchBar rounded
+        androidStatusBarColor='white'
+        style={{ backgroundColor: 'transparent' }}
+      >
+        <Item style={Styles.searchBar}>
+          <Icon name='search' />
+          <Input
+            placeholder="상품을 검색 해보세요"
+            placeholderTextColor={THEME_COLOR}
+            selectionColor={ICON_COLOR}
+            onChangeText={text => searchItem(text)} />
+        </Item>
+      </Header>
+           
+            <View>
+              {itemsFiltered.map((item, index) => (
+                <ListItem key={index} style={{ flexDirection: 'column', alignItems: 'flex-start' }} onPress={() => { toggleModal(item); setPausePressable(true); }}>
+                  {item.label == 'New' ?
+                    <Text color="primary" size="xx-small" boldness="normal" >{item.label.toUpperCase()}</Text>
+                    : <></>}
+                  <Text color="dark" size="medium" boldness="normal" >{item.name}</Text>
+                  <Text color="grey" size="small" boldness="normal">재고: {item.inventoryCount}개</Text>
+                  <Text color="lightGrey" decoration='strike-through'>가격: {currencyFormat(item.parentPrice)}원</Text>
+                  <Text color='dark'>가격: {currencyFormat(item.price)}원</Text>
+                </ListItem>
+
+              ))}
+            </View>
+    </Container >
+    
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
